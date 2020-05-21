a401dfa0b6c2ed5ec662bb108596ea31d8961228
Krista <48075080+k-allgood@users.noreply.github.com>
2020-05-21 18:52:55 -0400

    Initial upload

---
+++ script/app.js
@@ -0,0 +1,118 @@
+const canvas = document.getElementById("gamescreen");
+const ctx = canvas.getContext("2d");
+const gameWidth = 800;
+const gameHeight = 600;
+
+ctx.clearRect(0, 0, 800, 600);
+
+class Paddle {
+  constructor(gameWidth, gameHeight) {
+    this.width = 150;
+    this.height = 30;
+
+    this.position = {
+      x: gameWidth / 2 - this.width / 2,
+      y: gameHeight - this.height - 10,
+    };
+  }
+  draw(ctx) {
+    ctx.fillStyle = "slategray";
+    ctx.beginPath();
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+    ctx.closePath();
+  }
+}
+
+class Ball {
+  constructor() {
+    this.position = {
+      x: canvas.width / 2,
+      y: canvas.height - 50,
+      dx: 2,
+      dy: -2,
+    };
+    this.radius = 10;
+  }
+  draw(ctx) {
+    ctx.beginPath();
+    ctx.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+    ctx.fillStyle = "blue";
+    ctx.fill();
+    ctx.closePath();
+    this.position.x += this.position.dx;
+    this.position.y += this.position.dy;
+
+    //Checks if the radius of the ball collides with objects
+    if (
+      this.position.x + this.position.dx > canvas.width - this.radius ||
+      this.position.x + this.position.dx < this.radius
+    ) {
+      //Bounce off the left or right wall
+      this.position.dx = -this.position.dx;
+    }
+
+    if (this.position.y + this.position.dy < this.radius) {
+      //Bounce off the top wall
+      this.position.dy = -this.position.dy;
+    }
+    //Check if the ball hits the bottom wall
+    if (this.position.y + this.position.dy > canvas.height - this.radius) {
+      alert("GAME OVER");
+      document.location.reload();
+      clearInterval(interval); // Needed for Chrome to end game
+    }
+    //Check if the ball's y-cord is equal to the paddle's y -cord
+    if (this.position.y === paddle.position.y) {
+      //Check if the ball's x-cord is greater than the paddle's x-cord, and if it's less than the paddle width
+      if (
+        this.position.x > paddle.position.x &&
+        this.position.x < paddle.position.x + paddle.width
+      ) {
+        //Bounce off the paddle
+        this.position.dy = -this.position.dy;
+      }
+    }
+  }
+}
+
+let paddle = new Paddle(gameWidth, gameHeight);
+let ball = new Ball();
+
+paddle.draw(ctx);
+
+//Redraws the ball & paddle every 10ms
+let interval = setInterval(() => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ball.draw(ctx);
+  paddle.draw(ctx);
+}, 10);
+
+const eventHandler = () => {
+  document.addEventListener("keydown", (e) => {
+    switch (e.key) {
+      case "ArrowLeft":
+        //Move left
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        paddle.position.x -= 10;
+        //Stops paddle from leaving left edge of screen
+        if (paddle.position.x < 0) {
+          paddle.position.x = 0;
+        }
+        paddle.draw(ctx);
+        break;
+      case "ArrowRight":
+        //Move right
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        paddle.position.x += 10;
+        //Stops paddle from leaving right edge of screen
+        if (paddle.position.x > canvas.width - paddle.width) {
+          paddle.position.x = canvas.width - paddle.width;
+        }
+        paddle.draw(ctx);
+        break;
+      default:
+        return;
+    }
+  });
+};
+eventHandler();
